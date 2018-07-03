e081224d6c06967daed1c06239128a49fe62437a
Gabriela Antezana <gabiantezana@hotmail.com>
2018-07-03 18:49:43 -0500

    Working

--- YAMBOLY.GESTIONRUTAS/Areas/GeoLocation/Scripts/Testing.js
+++ YAMBOLY.GESTIONRUTAS/Areas/GeoLocation/Scripts/Testing.js
@@ -102,3 +102,28 @@ function UpdatePolygon(polygon) {
         match = polygon;
 }
 
+function GetPolygonCenter(poly) {
+    var lowx,
+        highx,
+        lowy,
+        highy,
+        lats = [],
+        lngs = [],
+        vertices = poly.getPath();
+
+    for (var i = 0; i < vertices.length; i++) {
+        lngs.push(vertices.getAt(i).lng());
+        lats.push(vertices.getAt(i).lat());
+    }
+
+    lats.sort();
+    lngs.sort();
+    lowx = lats[0];
+    highx = lats[vertices.length - 1];
+    lowy = lngs[0];
+    highy = lngs[vertices.length - 1];
+    center_x = lowx + ((highx - lowx) / 2);
+    center_y = lowy + ((highy - lowy) / 2);
+    return (new google.maps.LatLng(center_x, center_y));
+}
+
